547cbf5b00565f91d183d0c0b00bbdffad157ec1
Markus Kainu <markus.kainu@fao.org>
2015-07-02 11:31:51 +0200

    jekyll polished

--- toistettava/script.R
+++ toistettava/script.R
@@ -1,12 +1,14 @@
 #' This is a R-script that demonstrates how to create plots and tables
 #' Load the libraries first
 library(knitr)
-
-#' Create a plot
-
-plot(cars)
+library(ggplot2)
 
 #' Create a markdown table
-#' 
 
 kable(head(cars))
+
+#' You can also set chunk options like this
+#+ chunk-label, echo=FALSE
+
+ggplot(cars, aes(x=speed, y=dist, color=speed)) +
+  geom_point() + geom_smooth(method="loess")
